07929106aa2ed5fdefaba919f9853743fadbf85d
kozhushna <k.kozhushna@gmail.com>
2023-02-15 18:48:23 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const createGalleryItem = (preview, original, description) =>
+  `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+
+const renderList = img =>
+  img.reduce(
+    (acc, { preview, original, description }) =>
+      acc + createGalleryItem(preview, original, description),
+    ''
+  );
+
+const gallery = document.querySelector('.gallery');
+
+const insertGalleryItems = string => {
+  gallery.insertAdjacentHTML('beforeend', string);
+};
+
+const result = renderList(galleryItems);
+insertGalleryItems(result);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
