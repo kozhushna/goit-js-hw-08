2990ce7aa4e893f7ce6efbaafa92d95c2858e89e
kozhushna <k.kozhushna@gmail.com>
2023-02-17 12:18:06 +0200

    Add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const feedbackForm = document.querySelector('.feedback-form');
+
+const formInit = () => {
+  let formState = localStorage.getItem(STORAGE_KEY);
+  if (formState) {
+    formState = JSON.parse(formState);
+    Object.entries(formState).forEach(([name, value]) => {
+      feedbackForm.elements[name].value = value;
+    });
+  }
+};
+
+formInit();
+
+feedbackForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const { currentTarget } = e;
+  const formData = new FormData(currentTarget);
+  formData.forEach((value, name) => console.log(name, value));
+  currentTarget.reset();
+});
+
+feedbackForm.addEventListener(
+  'input',
+  throttle(e => {
+    let formState = localStorage.getItem(STORAGE_KEY);
+    formState = formState ? JSON.parse(formState) : {};
+    formState[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formState));
+  }, 500)
+);
+
+feedbackForm.addEventListener('reset', e => {
+  localStorage.removeItem(STORAGE_KEY);
+});
