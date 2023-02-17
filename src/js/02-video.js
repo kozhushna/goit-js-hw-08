a467967cf12a7b77a8ad8d9a59bcf0f8814d42bb
kozhushna <k.kozhushna@gmail.com>
2023-02-17 18:38:07 +0200

    Add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const initPlayer = () => {
+  let currentTime = localStorage.getItem(STORAGE_KEY);
+  if (currentTime) {
+    currentTime = parseFloat(currentTime);
+    player.setCurrentTime(currentTime);
+  }
+};
+
+initPlayer();
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('play', () => console.log('played the video!'));
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+  }, 1000)
+);
